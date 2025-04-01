be725d8b8285c398ee1d051a9018f422d5cf6ee7
arunkumarmudakavi <arunkumarmudakavi029@gmail.com>
2025-04-01 21:17:23 +0530

    Margin ruler component added

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -21,6 +21,7 @@ import { LineHeightExtension } from "@/extensions/line-height";
 import { useEditor, EditorContent } from "@tiptap/react";
 
 import { useEditorStore } from "@/app/store/use-editor-store";
+import { Ruler } from "./ruler";
 
 export const Editor = () => {
   const { setEditor } = useEditorStore();
@@ -107,6 +108,7 @@ export const Editor = () => {
   });
   return (
     <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <Ruler />
       <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
         <EditorContent editor={editor} />
       </div>
