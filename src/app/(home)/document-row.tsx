15b18acf5d03b1632eb4e7d667c0f6760944ca71
arunkumarmudakavi <arunkumarmudakavi029@gmail.com>
2025-04-08 18:46:41 +0530

    template-gallary: document rename, remove and open in new tab functionality added

--- src/app/(home)/document-row.tsx
+++ src/app/(home)/document-row.tsx
@@ -4,14 +4,25 @@ import { TableCell, TableRow } from "@/components/ui/table";
 import { format } from "date-fns";
 import { CircleUserIcon, Building2Icon, MoreVertical } from "lucide-react";
 import { SiGoogledocs } from "react-icons/si";
+import { DocumentMenu } from "./document-menu";
+import { useRouter } from "next/navigation";
 
 interface DocumentRowProps {
   document: Doc<"documents">;
 }
 
 export const DocumentRow = ({ document }: DocumentRowProps) => {
+  const router = useRouter();
+
+  const onNewTabClick = (id: string) => {
+    window.open(`/documents/${id}`, "_blank");
+  };
+
   return (
-    <TableRow className="cursor-pointer">
+    <TableRow
+      className="cursor-pointer"
+      onClick={() => router.push(`/documents/${document._id}`)}
+    >
       <TableCell className="w-[50px]">
         <SiGoogledocs className="size-6 fill-blue-500" />
       </TableCell>
@@ -28,9 +39,11 @@ export const DocumentRow = ({ document }: DocumentRowProps) => {
         {format(new Date(document._creationTime), "MMM dd, yyyy")}
       </TableCell>
       <TableCell className="flex justify-end">
-        <Button variant="ghost" size="icon" className="rounded-full">
-        <MoreVertical className="size-4"/>
-        </Button>
+        <DocumentMenu
+          documentId={document._id}
+          title={document.title}
+          onNewTab={onNewTabClick}
+        />
       </TableCell>
     </TableRow>
   );
